8d90086924b5c5fcfbf66d40b30a1065ae8338e6
mhlinder@work <mhlinder@gmail.com>
2014-04-07 14:59:12 -0400

    cleanup

--- antipodes.py
+++ antipodes.py
@@ -1,50 +1,100 @@
-from pandas import read_fwf
-from pyproj import Proj, transform
-from numpy import vstack
-from sklearn.neighbors import NearestNeighbors
+# courtesy of http://en.wikipedia.org/wiki/Antipodes#Cities
 
-# calculate lat & lon antipode
-def antipode(lon, lat):
-    newlat = -1*lat
-    if lon > 0:
-        newlon = lon - 180
+antipodes = [ ['Christchurch, New Zealand', 'A Coruna, Spain'],
+              ['Hamilton, New Zealand', 'Cordoba, Spain'],
+              ['Hong Kong', 'La Quiaca, Argentina'],
+              ['Junin, Argentina', 'Lianyungang, China'],
+              ['Padang, Indonesia', 'Esmeraldas, Ecuador'],
+              ['Palembang, Indonesia', 'Neiva, Colombia'],
+              ['Rafaela, Argentina', 'Wuhu, China'],
+              ['Segovia, Spain', 'Masterton, New Zealand'],
+              ['Tauranga, New Zealand', 'Jaen, Spain'],
+              ['Ulan Ude, Russia', 'Puerto Natales, Chile'],
+              ['Valdivia, Chile', 'Wuhai, China'],
+              ['Whangarei, New Zealand', 'Tangier, Morocco'],
+              ['Xian, China', 'Santiago, Chile'],
+              ['Auckland, New Zealand', 'Seville, Spain'],
+              ['Auckland, New Zealand', 'Setenil de las Bodegas, Spain'],
+              ['Auckland, New Zealand', 'Malaga, Spain'],
+              ['Tianjin, China', 'Bahia Blanca, Argentina'],
+              ['Shanghai, China', 'Salto, Uruguay'],
+              ['Taipei, Taiwan', 'Asuncion, Paraguay'],
+              ['Nanjing, China', 'Rosario, Argentina'],
+              ['Montpellier, France', 'Waitangi, New Zealand'],
+              ['Beijing, China', 'Bahia Blanca, Argentina'],
+              ['Beijing, China', 'Buenos Aires, Argentina'],
+              ['Shanghai, China', 'Buenos Aires, Argentina'],
+              ['Bogota, Colombia', 'Jakarta, Indonesia'],
+              ['Guayaquil, Ecuador', 'Medan, Indonesia'],
+              ['Phnom Penh, Cambodia', 'Lima, Peru'],
+              ['Dili, Timor-Leste', 'Paramaribo, Suriname'],
+              ['Irkutsk, Russia', 'Punta Arenas, Chile'],
+              ['Tongchuan, China', 'Licanten, Chile'],
+              ['Suva, Fiji', 'Timbuktu, Mali'],
+              ['Canberra, Australia', 'Azores, Portugal'],
+              ['Melbourne, Australia', 'Azores, Portugal'],
+              ['Cherbourg, France', 'Antipodes Islands'],
+              ['Pago Pago, American Samoa', 'Zinder, Niger'],
+              ['Barranquilla, Colombia', 'Christmas Island, Australia'],
+              ['Doha, Qatar', 'Pitcairn Island'],
+              ['Hue, Vietnam', 'Arequipa, Peru'],
+              ['Da Nang, Vietnam', 'Arequipa, Peru'],
+              ['Manila, Philippines', 'Cuiaba, Brazil'],
+              ['Kuala Lumpur, Malaysia', 'Cuenca, Ecuador'],
+              ['San Juan, Puerto Rico', 'Karratha, Australia'],
+              ['Limerick, Ireland', 'Campbell Islands, New Zealand'],
+              ['Arrecife', 'Norfolk Island'],
+              ['Lanzarote', 'Norfolk Island'],
+              ['Canary Islands', 'Norfolk Island'],
+              ['Sharm el Sheikh, Egypt', 'Rapa Iti, French Polynesia'],
+              ['Bangkok, Thailand', 'Lima, Peru'],
+              ['Quito, Ecuador', 'Singapore'],
+              ['Perth, Australia', 'Hamilton, Bermuda'],
+              ['Montevideo, Uruguay', 'Seoul, South Korea'] ]
+
+
+# unique locations
+locs = []
+for pair in antipodes:
+    for l in pair:
+        if l not in locs:
+            locs.append(l)
+
+# query api
+from requests import get
+from numpy import abs, argmin, nan
+import pickle, json
+
+# openweathermap.org
+appid = 'aa0bffa7bbd13e7350183cfcfd3c66b38'
+responses = {}
+for loc in locs:
+    responses[loc] = None
+    q = 'http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&units=imperial&APPID=%s' % (loc, appid)
+    r = get(q)
+    responses[loc] = r.json()
+pickle.dump(responses, open('data/responses.p', 'wb'))
+
+# responses = pickle.load(open('data/responses.p', 'rb'))
+
+temps = []
+diffs = []
+for pair in antipodes:
+    l1, l2 = pair
+    if len(responses[l1]['list']) > 0 and len(responses[l2]['list']) > 0:
+        t1 = responses[l1]['list'][0]['main']['temp']
+        t2 = responses[l2]['list'][0]['main']['temp']
+
+        temps.append([t1, t2])
+        diffs.append(abs(t1 - t2))
     else:
-        newlon = lon + 180
-    return (newlon, newlat)
+        temps.append([nan, nan])
+        diffs.append(200)
 
-# see http://www.wunderground.com/about/faq/international_cities.asp
-# for data specs
-stations = read_fwf('data/station_list', header=None,
-                widths = [26, 7, 5, 11, 9, 5, 5],
-                names = ['Station', 'Country', 'ID', 'Lat',
-                         'Lon', 'Elev', 'WMO'])
-locs = stations[['Lon', 'Lat']].values
+ix = argmin(diffs)
 
-# project into equirectangular, centered on antipode of each station
-p1 = Proj({'proj':'longlat', 'datum':'WGS84'})
+print '%s: %.1fF' % (antipodes[ix][0], temps[ix][0])
+print '%s: %.1fF' % (antipodes[ix][1], temps[ix][1])
 
-# indices of antipodes
-antipodes = []
-
-for i in range(len(stations)):
-    print i
-    station = stations.iloc[i]
-    
-    # project antipode
-    ant = antipode(station['Lon'], station['Lat'])
-    p2 = Proj({'proj': 'eqc', 'lon_0': ant[0]})
-    ant = transform(p1, p2, ant[0], ant[1])
-    
-    # project other points
-    points = vstack(transform(p1, p2, locs[:,0], locs[:,1])).T
-    nbrs = NearestNeighbors(n_neighbors = 5,
-            algorithm='ball_tree').fit(points)
-    
-    distances, indices = nbrs.kneighbors(ant)
-    distances = distances[0]
-    indices = indices[0]
-    
-    ixs = indices[distances < 150000]
-    for ix in ixs:
-        if [i, ix] not in antipodes and [ix, i] not in antipodes:
-            antipodes.append([i, ix])
+output = dict(zip(antipodes[ix], temps[ix]))
+json.dump(output, open('data/antipodes.json', 'w'))
